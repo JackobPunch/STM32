840419401c3b1ef7ce57cf4eed5ae201283d841f
JackobPunch <jakubcios99@gmail.com>
2024-07-31 18:18:57 +0200

    start_of_work_on_the_project

---
+++ My_workspace/target/401FreeRTOS_first/Debug/ThirdParty/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+ThirdParty/FreeRTOS/queue.o: ../ThirdParty/FreeRTOS/queue.c \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/queue.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/task.h
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/queue.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/task.h:
---
+++ My_workspace/target/401FreeRTOS_first/Debug/ThirdParty/FreeRTOS/stream_buffer.d
@@ -0,0 +1,22 @@
+ThirdParty/FreeRTOS/stream_buffer.o: \
+ ../ThirdParty/FreeRTOS/stream_buffer.c \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/stream_buffer.h
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/rolni/kody/STM32/My_workspace/target/401FreeRTOS_first/ThirdParty/FreeRTOS/include/stream_buffer.h:
